a4856677eea1c93eb19c4a5c804c78dd03898ae5
sophia-guo <sophiag@ca.ibm.com>
2018-11-30 14:37:10 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,8 +8,6 @@ int main()
         printf("on this line\n");
     else {
         printf("but not here\n");
-        printf("reduce covrage\n");
-        printf("reduce covrage\n");
     }
 
     return 0;
